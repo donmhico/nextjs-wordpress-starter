d60f64b8e3972b0bd2d72aa666ae1790ad3b5145
Michael Joseph Panaga <donmhico@gmail.com>
2021-03-30 11:48:21 +0800

    Bare react native entry point

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
+/**
+ *
+ */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>To share a photo from your phone with a friend, just press the button below!</Text>
+      <Text>
+        To share a photo from your phone with a friend, just press the button
+        below!
+      </Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -14,6 +20,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
